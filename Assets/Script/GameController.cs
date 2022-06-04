28c59a4a7cba46b4118bbee82a82579d3ab5dba4
Jaikishore <jaikishore1997@gmail.com>
2022-06-05 01:34:06 +0530

    Implemented tiles with atom animation

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public int width, height;
+    public GameObject tile, parentGameObject;
+    GameObject spawnTile;
+    Vector3 spawnPosition;
+    
+    void OnEnable() {
+        // SpawnTiles();
     }
-
-    // Update is called once per frame
-    void Update()
+    void SpawnTiles()
     {
-        
+        for(int i=0; i<width; i++){
+            for(int j=0; j<height; j++){
+                spawnPosition = new Vector3(i * (tile.GetComponent<RectTransform>().rect.width), j * (tile.GetComponent<RectTransform>().rect.height), 0f);
+                spawnTile = Instantiate(
+                    tile,
+                    spawnPosition,
+                    Quaternion.identity
+                );
+                spawnTile.transform.SetParent(parentGameObject.transform);
+                Debug.Log("Instantiate Position : "+spawnPosition, spawnTile);
+                // spawnTile.transform.position = spawnPosition;
+            }
+        }
     }
 }
---
+++ Assets/Script/TilesController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TilesController : MonoBehaviour
+{
+    public int noOfAtom;
+    public GameObject singleAtom,
+                        doubleAtom,
+                        tripleAtom;
+    GameObject currentActiveAtom;
+    private void Start() {
+        noOfAtom = 0;
+        GetComponent<Button>().onClick.AddListener(IncreaseAtom);
+    }
+    
+    void IncreaseAtom(){
+        if(currentActiveAtom != null){
+            currentActiveAtom.SetActive(false);
+        }
+        noOfAtom++;
+        switch(noOfAtom){
+            case 1:
+                singleAtom.SetActive(true);
+                currentActiveAtom = singleAtom;
+                break;
+            case 2:
+                doubleAtom.SetActive(true);
+                currentActiveAtom = doubleAtom;
+                break;
+            case 3:
+                tripleAtom.SetActive(true);
+                currentActiveAtom = tripleAtom;
+                break;
+            default:
+                noOfAtom = 0;
+                break;
+        }
+    }
+
+    void ReleaseAtom(){
+
+    }
+
+    private void OnCollisionStay2D(Collision2D other) {
+        Debug.Log("Collision : "+other.contacts.Length);
+    }
+}
