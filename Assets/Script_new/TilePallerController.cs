bf563e661fd184df60f2957191885df792b45784
Jaikishore <jaikishore1997@gmail.com>
2022-06-21 08:55:31 +0530

    Atom movement implementation and collision side detection

--- Assets/Script_new/TilePallerController.cs
+++ Assets/Script_new/TilePallerController.cs
@@ -6,12 +6,23 @@ public class TilePallerController : MonoBehaviour
 {
     public int atomCount { get; private set; }
     public AtomController movingAtom;
+    public List<AtomMoveDirection> collidedObjectDirection;
+    public bool execute;
     GameObject instantiatedObject;
     AtomController spawnedObject;
     int childCount;
+    Vector3 objectDirection, upDirection, rightDirection;
 
     private void Start() {
         atomCount = 0;
+        collidedObjectDirection = new List<AtomMoveDirection>();
+        upDirection = transform.up;
+        rightDirection = transform.right;
+        // Debug.Log("In start : ");
+        // Debug.Log("Up : "+(-transform.up));
+        // Debug.Log("Down : "+(-transform.right));
+        // Debug.Log("Left : "+transform.left);
+        // Debug.Log("Right : "+transform.right);
     }
 
     void DestroyChild(){
@@ -35,5 +46,30 @@ public class TilePallerController : MonoBehaviour
         DestroyChild();
         spawnedObject = Instantiate<AtomController>(movingAtom, gameObject.transform);
         spawnedObject.StartMovement(ChainGameController.instance.atomMovementSpeed, AtomMoveDirection.Right);
+    }
+
+    void GetDirection(Vector3 otherAtomDirection){
+        Debug.Log("Other Gameobject : "+otherAtomDirection);
+        Debug.Log("Down : "+(-(transform.up.normalized)));
+        Debug.Log("tranform up : "+(-(transform.up.normalized) == otherAtomDirection));
+        if(otherAtomDirection.Equals(transform.up.normalized)){
+            collidedObjectDirection.Add(AtomMoveDirection.Top);
+        }else if(otherAtomDirection.Equals(-(transform.up.normalized))){
+            collidedObjectDirection.Add(AtomMoveDirection.Bottom);
+        }else if(otherAtomDirection.Equals(transform.right.normalized)){
+            collidedObjectDirection.Add(AtomMoveDirection.Right);
+        }else if(otherAtomDirection.Equals(-(transform.right.normalized))){
+            collidedObjectDirection.Add(AtomMoveDirection.Left);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(!execute) { return; }
+        if(other.gameObject.TryGetComponent(out TilePallerController _)){
+            objectDirection = other.gameObject.transform.position - transform.position;
+            objectDirection.Normalize();
+            GetDirection(objectDirection);
+            Debug.Log(gameObject.name+" Collision Detected : "+other.gameObject.name+" Direction : "+objectDirection, other.gameObject);
+        }
     }
 }
